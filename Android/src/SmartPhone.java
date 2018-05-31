164592e5622c0fab99ea3aa6baab228d470dcc0d
GitHub jun-01731 <GitHub doonpelo@gmail.com>
2018-05-31 12:55:13 +0900

    531

---
+++ Android/src/SmartPhone.java
@@ -0,0 +1,16 @@
+
+public class SmartPhone extends Mp3Player{
+	public void call(){
+		System.out.println("電話");
+	}
+	public void mail(){
+		System.out.println("メール");
+	}
+	public void internet(){
+		System.out.println("インターネット");
+	}
+	public void photo(){
+		System.out.println("写真");
+	}
+
+}
---
+++ iPhone2/src/iPhone2/Iphone.java
@@ -0,0 +1,11 @@
+package iPhone2;
+
+public class Iphone {
+  public static void main(String[] args){
+	  SmartPhone iphone=new SmartPhone();
+	  iphone.play();
+	  iphone.stop();
+	  iphone.next();
+	  iphone.back();
+  }
+}
