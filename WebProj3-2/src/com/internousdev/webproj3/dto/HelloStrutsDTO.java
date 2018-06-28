0bb139b8ffe74777e3ed7927ecbc5573167ba662
jun-01731 <dooonpelo@gmail.com>
2018-06-28 10:23:05 +0900

    627

---
+++ WebProj3-2/src/com/internousdev/webproj3/dto/HelloStrutsDTO.java
@@ -0,0 +1,14 @@
+package com.internousdev.webproj3.dto;
+
+public class HelloStrutsDTO {
+
+	private String result;
+
+	public String getResult() {
+		return this.result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+}
